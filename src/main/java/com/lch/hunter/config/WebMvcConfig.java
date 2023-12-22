879ca5e0c0534e21be61881fb69a9690f96b1625
MarBarb <135206933+MarBarb@users.noreply.github.com>
2023-12-22 20:59:29 +0800

    添加登录的拦截器

--- src/main/java/com/lch/hunter/config/WebMvcConfig.java
+++ src/main/java/com/lch/hunter/config/WebMvcConfig.java
@@ -6,9 +6,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new LoginInterceptor());//.addPathPatterns("/user/**");
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器，并指定拦截的路径
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/require/**"); // 拦截以 /secure/ 开头的路径
     }
 }
--- src/main/java/com/lch/hunter/interceptor/LoginInterceptor.java
+++ src/main/java/com/lch/hunter/interceptor/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.lch.hunter.interceptor;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("拦截！");
+        // 检查是否是HandlerMethod
+        if (handler instanceof HandlerMethod handlerMethod) {
+
+            // 检查方法上是否有 @PostMapping 注解
+            if (handlerMethod.hasMethodAnnotation(PostMapping.class)) {
+                // 在这里可以进行登录验证逻辑，如果用户未登录，则拦截请求
+                // 例如，可以检查用户是否具有登录的凭证（例如，session中是否有用户信息）
+
+                // 示例：检查 session 中是否有用户信息
+                Object user = request.getSession().getAttribute("user");
+                if (user == null) {
+                    // 用户未登录，拦截请求
+                    System.out.println("拦截！！！");
+                    response.sendRedirect("/signin"); // 重定向到登录页面
+                    return false; // 请求被拦截
+                }
+            }
+        }
+
+        // 如果不是 @PostMapping 请求或用户已登录，放行请求
         return true;
     }
 
+    // 其他方法保持不变
 }
