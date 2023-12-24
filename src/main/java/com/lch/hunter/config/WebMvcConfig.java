44f242c11ba5bc6761df8a6b6f54fdc2baa82760
TRYJL <116693886+TRYJL@users.noreply.github.com>
2023-12-24 14:45:45 +0800

    Update WebMvcConfig.java

--- src/main/java/com/lch/hunter/config/WebMvcConfig.java
+++ src/main/java/com/lch/hunter/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器，并指定拦截的路径
-        registry.addInterceptor(new LoginInterceptor())
-                .addPathPatterns("/require/**"); // 拦截以 /secure/ 开头的路径
+//        registry.addInterceptor(new LoginInterceptor())
+//                .addPathPatterns("/require/**"); // 拦截以 /secure/ 开头的路径
     }
 }
