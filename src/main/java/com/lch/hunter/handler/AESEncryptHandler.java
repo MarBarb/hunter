dc022d7e92abbaa4008b3a13b83ef31b2054a796
MarBarb <135206933+MarBarb@users.noreply.github.com>
2023-12-26 16:19:21 +0800

    Update AESEncryptHandler.java

--- src/main/java/com/lch/hunter/handler/AESEncryptHandler.java
+++ src/main/java/com/lch/hunter/handler/AESEncryptHandler.java
@@ -3,15 +3,20 @@ package com.lch.hunter.handler;
 import com.baomidou.mybatisplus.core.toolkit.AES;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
 //AES 是工具方法类，按加密需求设置
 public class AESEncryptHandler extends BaseTypeHandler {
-    private String AESKey = "aaOVL1xnJW+1dLlj";
+    private String AESKey = "aaOVL1xnJW+1dLlj"; // 密钥定期更换（注意更换密钥的同时需要重新加密数据库密码）
 
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
@@ -28,8 +33,7 @@ public class AESEncryptHandler extends BaseTypeHandler {
         return AES.decrypt(columnValue, AESKey);
     }
     @Override
-    public String getNullableResult(CallableStatement cs, int columnIndex)
-            throws SQLException {
+    public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         String columnValue = cs.getString(columnIndex);
         return AES.decrypt(columnValue, AESKey);
     }
